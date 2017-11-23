1e64c035077056358642cd548cf702f72325bac1
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2017-11-23 18:19:22 +0800

    Setup homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,108 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import get from 'lodash/get'
+import React, { PureComponent } from 'react'
+import styled from 'styled-components'
 import Helmet from 'react-helmet'
+import get from 'lodash/get'
+import avatar from '../../static/img/avatar.svg'
 
-class BlogIndex extends React.Component {
+import { BannerContainer } from '../components/Banner'
+import Wrapper from '../components/Wrapper'
+import * as Icon from 'react-feather'
+import { ButtonLink } from '../components/Button'
+
+const HomeBanner = BannerContainer.extend`
+  background: #e1e8ed;
+`
+
+const Hero = styled.section`
+  display: flex;
+`
+
+const Avatar = styled.div`
+  align-items: flex-end;
+  flex: 1;
+
+  & > img {
+    display: flex;
+    width: 15em;
+    margin-right: 3em;
+  }
+`
+
+const Copy = styled.div`
+  align-self: flex-end;
+  padding-bottom: 3.5em;
+  text-align: left;
+
+  & > h1 {
+    font-size: 1.85em;
+    font-weight: 600;
+    margin: 0;
+    opacity: 0.9;
+  }
+
+  & > p {
+    font-size: 1.15em;
+    opacity: 0.8;
+    line-height: 1.5;
+    margin: 0 0 0.6em;
+  }
+`
+
+const Actions = styled.div`
+  margin-top: 1.5em;
+
+  & > ${ButtonLink} {
+    margin: 0.25em 0.5em 0.25em 0;
+    min-width: 7.15em;
+  }
+`
+
+class Home extends PureComponent {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
-
     return (
-      <div>
-        <Helmet title={siteTitle} />
-        {posts.map(({ node }) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3>
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          )
-        })}
-      </div>
+      <HomeBanner>
+        <Helmet title={`${siteTitle} ∙ Creative Developer`} />
+        <Wrapper>
+          <Hero>
+            <Avatar>
+              <img draggable="false" src={avatar} />
+            </Avatar>
+            <Copy>
+              <h1>Hello there!</h1>
+              <p>
+                I'm Nelo — a software developer and UI designer currently
+                based in the Philippines.
+              </p>
+              <p>
+                I team up with startups and agencies around the world to
+                turn ideas into delightful digital products.
+              </p>
+              <Actions>
+                <ButtonLink to="/work">
+                  <Icon.Monitor />
+                  View works
+                </ButtonLink>
+                <ButtonLink to="/contact" inverted>
+                  <Icon.Mail />
+                  Contact me
+                </ButtonLink>
+              </Actions>
+            </Copy>
+          </Hero>
+        </Wrapper>
+      </HomeBanner>
     )
   }
 }
 
-export default BlogIndex
+export default Home
 
 export const pageQuery = graphql`
-  query IndexQuery {
+  query HomeQuery {
     site {
       siteMetadata {
         title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "DD MMMM, YYYY")
-            title
-          }
-        }
       }
     }
   }
