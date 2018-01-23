f8dd70fe3c303bc91f7d0f653b1158ecd46834c0
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-01-23 16:28:36 +0800

    Add wide, inverted and icon Button types

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,41 +3,48 @@ import Link from 'gatsby-link'
 import styled from 'styled-components'
 
 const Button = styled.button`
-  background: ${props => props.transparent ? 'none' :
-    props.inverted ? props.theme.white :
-    props.secondary ? props.theme.secondary :
-    props.theme.primary};
+  align-items: center;
+  background: ${props =>
+		props.transparent
+			? 'none'
+			: props.inverted
+				? props.theme.white
+				: props.secondary ? props.theme.secondary : props.theme.primary};
   border: none;
-  border-radius: ${props => props.sharp ? 0 : props.theme.borderRadius};
-  box-shadow: ${props => props.transparent ? 'none' : '0 1px 1px rgba(0, 0, 0, 0.1)'};
-  color: ${props => props.transparent || props.inverted ?
-    props.secondary ? props.theme.secondary :
-    props.theme.primary : props.theme.white};
+  border-radius: ${props => (props.sharp ? 0 : props.theme.borderRadius)};
+  box-shadow: ${props =>
+		props.transparent ? 'none' : '0 1px 1px rgba(0, 0, 0, 0.1)'};
+  box-sizing: border-box;
+  color: ${props =>
+		props.transparent || props.inverted
+			? props.secondary ? props.theme.secondary : props.theme.primary
+			: props.theme.white};
   cursor: pointer;
   display: inline-flex;
-  align-items: center;
-  font-size: 1em;
-  font-weight: 500;
-  margin-right: 0.5em;
-  outline: none;
-  padding: ${props => props.icon ? '0.7em' : '0.5em 1.5em'};
-  text-decoration: none;
-  transition: all .2s ease;
+  font-size: 0.95em;
+	font-weight: 400;
+	justify-content: ${props => props.wide ? 'center' : null};
+  line-height: 1.2;
+	margin-right: ${props => props.wide ? null : '0.5em'};
+  padding: ${props => (props.icon ? '1em' : props.wide ? '0.9em 1.25em 1em' : '0.55em 1.25em 0.65em')};
+	text-decoration: none;
+	transform: translateY(0);
+  transition: all 0.2s ease;
 
   & > svg {
-    margin-left: ${props => props.icon ? '0' : '-0.125em'};
-    margin-right: ${props => props.icon ? '0' : '0.75em'};
-    opacity: ${props => props.icon ? '1' : '0.8'};
-    height: 1.15em;
+    margin-left: ${props => (props.icon ? '0' : '-0.125em')};
+    margin-right: ${props => (props.icon ? '0' : '0.75em')};
+    opacity: ${props => (props.icon ? '1' : '0.8')};
+    height: 1em;
     position: relative;
-    top: -0.0612em;
   }
 
   &:hover {
-    background: ${props => props.transparent ? 'rgba(0, 0, 0, 0.025)' : null};
-    box-shadow: ${props => props.transparent ? null : '0 2px 4px rgba(0, 0, 0, 0.075)'};
-    transform: ${props => props.transparent ? null : 'translateY(-1px)'};
-  }
+    background: ${props => (props.transparent ? 'rgba(0, 0, 0, 0.025)' : null)};
+    box-shadow: ${props =>
+		props.transparent ? null : '0 2px 4px rgba(0, 0, 0, 0.075)'};
+    transform: ${props => (props.transparent ? null : props.wide ? 'translateY(-2px)' : 'translateY(-1px)')};
+	}
 
   &:active {
     box-shadow: none;
