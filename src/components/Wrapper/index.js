0b7a015466057866d9de3421bd62e924df5ac62f
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-01-24 15:29:27 +0800

    Make Wrapper component responsive

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -2,9 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
+  box-sizing: border-box;
   margin: 0 auto;
   max-width: ${props => props.theme.containerWidth};
   width: 100%;
+
+  @media (max-width: 42em) {
+    padding-left: 1em;
+    padding-right: 1em;
+  }
 `
 
 export default Wrapper
