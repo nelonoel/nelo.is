2cfdbe5e46439baea7313a4e9777dab246b22a9f
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-01-31 10:57:42 +0800

    Reuse Card component on project previews

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -4,13 +4,7 @@ import Helmet from 'react-helmet'
 
 import Wrapper from '../../components/Wrapper'
 import Banner, { Title, Description } from '../../components/Banner'
-import ProjectPreview from '../../components/Project/Preview'
-
-const ProjectWrapper = Wrapper.extend`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-`
+import Card, { Grid } from '../../components/Card'
 
 class WorkIndex extends React.Component {
   render() {
@@ -24,19 +18,22 @@ class WorkIndex extends React.Component {
           <Title>Work</Title>
           <Description>Blood, sweat, and tears over the years. I'm currently working on my own product called <a href="//timecheck.io">Timecheck</a> – a smart, passive time tracker.</Description>
         </Banner>
-        <ProjectWrapper>
-          {posts.map(({ node }) => {
-            return <ProjectPreview
-                slug={node.fields.slug}
-                cover={get(node, 'frontmatter.cover.childImageSharp.resize.src')}
-                title={get(node, 'frontmatter.title') || node.fields.slug}
-                subtitle={get(node, 'frontmatter.subtitle')}
-                category={get(node, 'frontmatter.category')}
-                type={get(node, 'frontmatter.type')}
-                date={get(node, 'frontmatter.date')}
-              />
-          })}
-        </ProjectWrapper>
+        <Wrapper>
+          <Grid itemMinWidth={'13em'}>
+            {posts.map(({ node }) => {
+              return <Card
+                  half={true}
+                  slug={node.fields.slug}
+                  cover={get(node, 'frontmatter.cover.childImageSharp.resize.src')}
+                  title={get(node, 'frontmatter.title') || node.fields.slug}
+                  subtitle={get(node, 'frontmatter.subtitle')}
+                  category={get(node, 'frontmatter.category')}
+                  type={get(node, 'frontmatter.type')}
+                  date={get(node, 'frontmatter.date')}
+                />
+            })}
+          </Grid>
+        </Wrapper>
       </div>
     )
   }
