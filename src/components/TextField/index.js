19f1e19ffe7d973c64739ea3246c2fe6866737eb
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-03-15 09:23:43 +0800

    Refine TextField styles

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -3,15 +3,15 @@ import { darken } from 'polished'
 
 const TextField = styled.input`
   background: ${props => props.theme.dark2};
-  border: none;
+  border: transparent solid 1px;
   border-radius: ${props => props.theme.borderRadius};
   color: ${props => props.theme.text};
 	font-size: 1.25em;
-	padding: 0.75em 1em;
+	padding: 0.75em;
 
 	&:focus {
 		background: ${props => darken(0.025, props.theme.dark2)};
-		box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.025);
+    border-color: ${props => darken(0.0375, props.theme.dark2)};
 	}
 `
 
