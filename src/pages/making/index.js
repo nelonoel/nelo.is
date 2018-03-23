2afd8c58c2a55d4d55fcb2f1f2cd6f412a09e447
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-03-23 23:57:16 +0800

    Revise Work Card date format  - Edit Project page copy

--- src/pages/making/index.js
+++ src/pages/making/index.js
@@ -7,36 +7,39 @@ import Banner, { Title, Description } from '../../components/Banner'
 import Card, { Grid } from '../../components/Card'
 
 class ProjectsIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+	render() {
+		const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+		const posts = get(this, 'props.data.allMarkdownRemark.edges')
 
-    return (
-      <div>
-        <Helmet title={`${siteTitle} ∙ Projects`} />
-        <Banner>
-          <Title>Projects</Title>
-          <Description>Blood, sweat, and tears over the years. I'm currently working on my own product called <a href="//timecheck.io">Timecheck</a> – a smart, passive time tracker.</Description>
-        </Banner>
-        <Wrapper>
-          <Grid itemMinWidth={'13em'}>
-            {posts.map(({ node }) => {
-              return <Card
-                  half={true}
-                  slug={node.fields.slug}
-                  cover={get(node, 'frontmatter.cover.childImageSharp.resize.src')}
-                  title={get(node, 'frontmatter.title') || node.fields.slug}
-                  subtitle={get(node, 'frontmatter.subtitle')}
-                  category={get(node, 'frontmatter.category')}
-                  type={get(node, 'frontmatter.type')}
-                  date={get(node, 'frontmatter.date')}
-                />
-            })}
-          </Grid>
-        </Wrapper>
-      </div>
-    )
-  }
+		return (
+			<div>
+				<Helmet title={`${siteTitle} ∙ Projects`} />
+				<Banner>
+					<Title>Projects</Title>
+					<Description>
+						Here are some of my finest work. <br />
+						Tools used to build these are listed <a href="/using">here</a>.
+					</Description>
+				</Banner>
+				<Wrapper>
+					<Grid itemMinWidth={'13em'}>
+						{posts.map(({ node }) => {
+							return <Card
+								half={true}
+								slug={node.fields.slug}
+								cover={get(node, 'frontmatter.cover.childImageSharp.resize.src')}
+								title={get(node, 'frontmatter.title') || node.fields.slug}
+								subtitle={get(node, 'frontmatter.subtitle')}
+								category={get(node, 'frontmatter.category')}
+								type={get(node, 'frontmatter.type')}
+								date={get(node, 'frontmatter.date')}
+							/>
+						})}
+					</Grid>
+				</Wrapper>
+			</div>
+		)
+	}
 }
 
 export default ProjectsIndex
@@ -64,13 +67,13 @@ export const pageQuery = graphql`
               childImageSharp {
                 resize {
                   src
-                }
+								}
               }
             }
             model
             category
             type
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "MMM YYYY")
           }
         }
       }
