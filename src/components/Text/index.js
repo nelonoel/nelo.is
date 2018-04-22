0efb43187c0ff1d470acf9d22da05c97b4596c90
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-04-22 22:41:04 +0800

    Add Text component

---
+++ src/components/Text/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+import {
+	color,
+	display,
+	fontSize,
+	fontWeight,
+	lineHeight,
+	space,
+	textAlign,
+	width
+} from 'styled-system'
+
+const Text = styled.div`
+	${color}
+	${display}
+	${fontSize}
+	${fontWeight}
+	${lineHeight}
+	${space}
+	${textAlign}
+	${width}
+`
+
+export default Text
