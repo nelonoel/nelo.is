36f3c9e5ba10fef3ba27921a220126cd666be9e7
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-04-23 00:34:14 +0800

    Convert Flex, Box to styled-system components

--- src/components/Flex/index.js
+++ src/components/Flex/index.js
@@ -1,22 +1,30 @@
 import styled from 'styled-components'
 import {
+	alignItems,
 	display,
+	flex,
 	flexDirection,
 	flexWrap,
+	justifyContent,
 	lineHeight,
 	position,
 	space,
 	textAlign
 } from 'styled-system'
 
-const Box = styled.div`
+const Flex = styled.div`
+	display: flex;
+
+	${alignItems}
 	${display}
+	${flex}
 	${flexDirection}
 	${flexWrap}
+	${justifyContent}
 	${lineHeight}
 	${position}
 	${space}
 	${textAlign}
 `
 
-export default Box
+export default Flex
