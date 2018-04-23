0e43e87792f77abf140ce08cce6aeb02740577e7
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-04-23 19:01:58 +0800

    Make custom Grid component responsive

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,7 +1,11 @@
 import Grid from 'react-css-grid'
 import styled from 'styled-components'
-import { space } from 'styled-system'
+import { space, theme } from 'styled-system'
 
 export default styled(Grid) `
 	${space}
+
+	@media (max-width: ${theme('breakpoints.1')}) {
+		grid-template-columns: auto;
+	}
 `
