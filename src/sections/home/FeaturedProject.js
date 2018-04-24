da18f7cf0b2e10f2ae66747c230d60c9a3d0079f
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-04-24 19:45:07 +0800

    Refine landing page copy

--- src/sections/home/FeaturedProject.js
+++ src/sections/home/FeaturedProject.js
@@ -21,7 +21,7 @@ export default class FeaturedProject extends PureComponent {
 					<Wrapper pt={5} textAlign="center">
 						<Subheading mt={0} mb={2}>Featured Project</Subheading>
 						<Text color="contrast.4" fontSize={4} fontWeight="bold" lineHeight={1}>Phaxio</Text>
-						<Text color="contrast.3" fontSize={2} lineHeight={1} mt={1} mx="auto" maxWidth="20em">A developer-friendly API that enables sending and receiving of faxes.</Text>
+						<Text color="contrast.3" fontSize={2} lineHeight={1} mt={1} mx="auto" maxWidth="20em">A developer-friendly API that enables processing of faxes.</Text>
 						<Box my={3} textAlign="center">
 							<ExternalButtonLink m="0.25em" minWidth="10.25em" href="//phaxio.com" target="_blank" rel="nofollow" inverted secondary>
 								<ExternalLink />
