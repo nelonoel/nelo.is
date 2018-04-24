880299c8e4f3a743a9c91174a151de0d055ee696
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-04-24 20:24:30 +0800

    Replace focus-visible package with dummy module during server render

--- gatsby-node.js
+++ gatsby-node.js
@@ -59,3 +59,12 @@ exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
 		})
 	}
 }
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+	if (stage === "build-html") {
+		config.loader("null", {
+			test: /focus-visible/,
+			loader: "null-loader",
+		});
+	}
+};
