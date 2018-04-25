b7887e52138ad102a26445e304cb4f8fbefbed67
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-04-25 12:10:33 +0800

    Convert mixins to plain strings

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -1,13 +1,11 @@
-import { css } from 'styled-components'
-
-export const fullWidth = css`
+export const fullWidth = `
 	box-sizing: border-box;
 	margin-left: 50%;
 	transform: translateX(-50%);
 	width: 100vw;
 `
 
-export const cover = css`
+export const cover = `
 	bottom: 0;
 	left: 0;
 	position: absolute;
