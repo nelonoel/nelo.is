ef56427d18e5a465bde142b8939e753f5314ae85
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-04-27 12:09:01 +0800

    Add GA Tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-google-analytics`,
 			options: {
-				//trackingId: `ADD YOUR TRACKING ID HERE`,
+				trackingId: `UA-77418752-3`,
 			},
 		},
 		`gatsby-plugin-feed`,
