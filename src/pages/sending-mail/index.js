7e6b5ba41f5f5b87737fa5ec771f76c90d978150
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-06-06 15:00:50 +0800

    Add subscription confirmation page

---
+++ src/pages/sending-mail/index.js
@@ -0,0 +1,22 @@
+import React, { PureComponent } from 'react'
+import Banner, { Emoji, Title, Description } from '../../components/Banner'
+import Flex from '../../components/Flex'
+import Link from 'gatsby-link'
+
+class ThanksPage extends PureComponent {
+	render() {
+		return (
+			<Flex alignItems="center" flex="1 1 auto" justifyContent="center">
+				<Banner>
+					<Emoji>🎉</Emoji>
+					<Title>Welcome to the party!</Title>
+					<Description>
+						Thank you for subscribing. <Link to="/writing">Continue reading</Link>?
+					</Description>
+				</Banner>
+			</Flex>
+		)
+	}
+}
+
+export default ThanksPage
