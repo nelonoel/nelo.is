a6ceaf34e7936c9d39b8158f5bb794a069ffb326
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2019-05-21 21:21:56 +0800

    Use OS-level dark mode as default when available

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -2,6 +2,7 @@ import React, { Fragment, createContext, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import Twemoji from 'react-twemoji'
 import store from 'store'
+import useEventListener from '@use-it/event-listener'
 
 import themes from '../../styles/theme'
 import GlobalStyles from '../../styles/global'
@@ -9,9 +10,18 @@ import GlobalStyles from '../../styles/global'
 export const ThemeContext = createContext()
 
 const Theme = props => {
-	const [theme, setTheme] = useState(store.get('theme') || 'light')
-	const { children } = props
+	const mql = global.matchMedia ? global.matchMedia('(prefers-color-scheme: dark)') : {}
+	const [theme, setTheme] = useState(store.get('theme') || mql.matches ? 'dark' : 'light')
 
+	useEventListener(
+		'change',
+		({ matches }) => setTheme(matches ? 'dark' : 'light'), {
+			addEventListener: (_, handler) => mql.addListener && mql.addListener(handler),
+			removeEventListener: (_, handler) => mql.removeListener && mql.removeListener(handler)
+		}
+	)
+
+	const { children } = props
 	const toggleTheme = name => {
 		store.set('theme', name)
 		setTheme(name)
